8af58f352ff13a18bb6f4514d0b7abb444665bdd
Carla C <skapkun@gmail.com>
2022-12-29 21:20:24 -0400

    vetor tipo referencia

---
+++ arrays, listas/introduçãoArray/src/application/Program.java
@@ -0,0 +1,37 @@
+package application;
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Program {
+	public static void main (String [] args) {
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner (System.in);
+		
+		int n; 
+		System.out.print("Entre com a quantidade de elementos do vetor: ");
+		n = sc.nextInt();
+	
+		double vect []  = new double [n];
+		
+		for (int i = 0; i < n; i++) {
+			System.out.printf("Entre com o elemento %d do vetor: ", i);
+			vect[i] = sc.nextDouble();
+		}
+		
+		double sum = 0.0;
+		for (int i = 0; i < n; i++) {
+			sum += vect[i]; 
+		 
+		}
+		
+		double media = sum/n;
+		
+		for (int i = 0; i < n; i++) {
+			System.out.printf("\n[%d]: %.2f", i, vect[i]);
+		 
+		}
+		
+		System.out.printf("\nMedia: %.2f", media);
+		sc.close(); 
+	}
+}
