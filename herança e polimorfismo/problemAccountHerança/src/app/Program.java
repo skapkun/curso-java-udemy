110a8576495a09b077ab5c33732d95bfd12627c1
Carla C <skapkun@gmail.com>
2023-01-02 20:20:49 -0400

    palavra super, sobreposição, override, classes e métodos final

--- herança e polimorfismo/problemAccountHerança/src/app/Program.java
+++ herança e polimorfismo/problemAccountHerança/src/app/Program.java
@@ -20,13 +20,19 @@ public class Program {
 		double balance = sc.nextDouble();
 		
 		*/
-		Account acc = new Account (1001, "Alex", 0.0);
+		Account acc = new Account (1001, "Alex", 1000.0);
+		acc.withdraw(200.0);
+		System.out.println(acc.getBalance());
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0); 
-		
+		Account Sacc2 = new SavingsAccount (1002, "Maria", 1000.0, 0.01);
+		Sacc2.withdraw(200.00);
+		System.out.println(Sacc2.getBalance());
 		//UPCASTING
 		
 		Account acc1 = bacc; 
-		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc2 = new BusinessAccount(1003, "Bob", 1000.0, 500.0);
+		acc2.withdraw(200);
+		System.out.println(acc2.getBalance());
 		Account acc3 = new SavingsAccount (1004, "Anna", 0.0, 0.01); 
 		acc1.getBalance();
 		
